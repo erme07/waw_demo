dc4a3b1d60b994584bb93b7830bbda849cd5c6ee
erme07 <erik.medina07@gmail.com>
2025-11-17 15:31:22 -0300

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ $toggleMenu.addEventListener("click", ()=>{
     $navbar.classList.toggle("show")
     $toggleMenu.classList.toggle("show")
 })
+
+import WAW from "https://cdn.jsdelivr.net/npm/waw-widget/dist/waw.min.js";
+
+const widget = new WAW();
+widget.run();
